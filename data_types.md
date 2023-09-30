36102d37e36053765c778d1be38cfd78b5a35c78
tedirland <irlandth@gmail.com>
2023-09-30 13:12:23 -0500

    added data types.py and cheat sheet

---
+++ data_types.md
@@ -0,0 +1,34 @@
+# Primitive Data Type Cheat Sheet
+
+## Strings  
+**Something meant for humans.**
+Made by putting in  `" "` or `' '`
+`"a"`
+`"apple"`
+`"3"`
+`"130 kilometers"`
+
+## Numbers
+
+### Integers
+
+A number with no decimal place
+
+`1`
+`115`
+`-6`
+
+### Floats
+A Number with a decimal place
+`3.14`
+`-0.4`
+`10000.1`
+
+### Booleans
+
+True or False 
+
+`True`
+`False`
+
+
---
+++ data_types.py
@@ -0,0 +1,28 @@
+# Like any programming languages - Python classifies variables into different types
+
+
+# Primitave
+
+book = "The Three Muskateers" # string
+# numbers
+my_int = 4 # integer (no decimal)
+my_float = 4.0 #float (decial)
+
+my_bool = True #boolean
+
+really_long_text_data_type = type(book)
+print(really_long_text_data_type)
+
+# Non-primitave 
+
+# Lists - list of variables 
+
+# real life analogy - a shelf 
+
+# Dict - a library
+
+# Object
+
+
+
+
