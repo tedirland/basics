27dd19edd0dbbb3667a2874873db422e20297ed6
tedirland <irlandth@gmail.com>
2023-09-30 12:48:14 -0500

    Added variables and test files

---
+++ test.py
@@ -0,0 +1,6 @@
+print( "Hello students" )
+print(3+5)
+Hellothere = 'Hi!'
+print(f'{Hellothere}')
+
+print("X")
---
+++ variables.py
@@ -0,0 +1,32 @@
+# Comments = text for the developer, not for Python
+
+#Variable = a place you can put data
+book = "The Count of Monte Cristo" #string
+print(book)
+page_count = 1276 #integer
+print(page_count)
+page_count = 100 #re-assigning pages variable
+print(page_count)
+page_count = page_count - 2 # remove two pages
+print(f"Removing {2} pages...")
+print("New page count is", page_count)
+page_count = page_count + 2 # add two pages
+print(f"Adding {2} pages...")
+print("New page count is", page_count)
+
+
+name = "Ted"
+name = 'Ted'
+
+
+
+
+
+#Books
+
+# Bookshelf
+
+# Library
+
+
+
